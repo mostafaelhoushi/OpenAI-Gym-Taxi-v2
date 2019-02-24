2428fc9d26850025e1a5855ef9938f470413991d
Mostafa Elhoushi <mostafa.elhoushi@gmail.com>
2019-02-24 16:03:04 -0500

    Pass argument to specify update method

--- workspace/main.py
+++ workspace/main.py
@@ -2,7 +2,22 @@ from agent import Agent
 from monitor import interact
 import gym
 import numpy as np
+import sys
 
+update_method = None
+if (len(sys.argv) > 1):
+    if(sys.argv[1] == "SARSA"):
+        update_method = Agent.UpdateMethod.SARSA
+    elif(sys.argv[1] == "SARSA_MAX"):
+        update_method = Agent.UpdateMethod.SARSA_MAX
+    elif(sys.argv[1] == "EXPECTED_SARSA"):
+        update_method = Agent.UpdateMethod.EXPECTED_SARSA
+    else:
+        raise ValueError("update method should be either: SARSA, SARSA_MAX, or EXPECTED_SARSA")
+else:
+    update_method = Agent.UpdateMethod.SARSA_MAX
+
+        
 overall_avg_reward = 0
 N_trials = 100
 
